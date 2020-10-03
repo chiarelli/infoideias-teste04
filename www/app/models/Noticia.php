1bc80870085e49d59e057863a659ec9f462ee2ba
Raphael <oi.sucesso@gmail.com>
2020-10-02 23:05:32 -0300

    Implementando Model Notícia

--- www/app/models/Noticia.php
+++ www/app/models/Noticia.php
@@ -1,17 +1,81 @@
 <?php
 
-
-
+use Phalcon\Db\Column;
 use Phalcon\Mvc\Model;
-use Phalcon\Paginator\Adapter\Model as Paginator;
 
-class Noticia extends Model
-{
+class Noticia extends Model {
 
-    public function initialize()
-    {
-        $this->setSource("noticia");
+    /**
+     *
+     * @var integer
+     * @Primary
+     * @Identity
+     * @Column(type="integer", length=11, nullable=false)
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", length=500, nullable=true)
+     */
+    public $titulo;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=true)
+     */
+    public $texto;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=true)
+     */
+    public $data_ultima_atualizacao;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=true)
+     */
+    public $data_cadastro;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize() {
+        $this->setSchema("phalcont_teste04");
     }
 
-    
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource() {
+        return 'noticia';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Noticia[]|Noticia
+     */
+    public static function find($parameters = null) {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Noticia
+     */
+    public static function findFirst($parameters = null) {
+        return parent::findFirst($parameters);
+    }
+
 }
