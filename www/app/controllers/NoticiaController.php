4d943b27d1e156d3126968174ba0325c21b30ff5
Raphael <oi.sucesso@gmail.com>
2020-10-02 23:10:08 -0300

    Ação de criação de Nova Notícia

--- www/app/controllers/NoticiaController.php
+++ www/app/controllers/NoticiaController.php
@@ -1,36 +1,76 @@
 <?php
 
+use Phalcon\Flash\Session as FlashSession;
 
-class NoticiaController extends ControllerBase
-{
-    private $mensagemDeErro = '';
+class NoticiaController extends ControllerBase {
 
-    public function listaAction()
-    {
+    /**
+     *
+     * @var FlashSession 
+     */
+    private $flash;
+
+    public function listaAction() {
 
         $this->view->pick("noticia/listar");
     }
 
-    public function cadastrarAction()
-    {
-        
-        $this->view->pick("noticia/cadastrar");
+    public function cadastrarAction() {
 
+        $this->view->pick("noticia/cadastrar");
     }
 
-    public function editarAction($id)
-    {
+    public function editarAction($id) {
         $this->view->pick("noticia/editar");
     }
 
-    public function salvarAction()
-    {
+    public function salvarAction() {
+        if (false === $this->request->isPost()) {
+            // Não faz nada, sai silenciosamente
+            return;
+        }
+
+        $form = new NoticiaUpdateForm();
+
+        if (!$form->isValid($this->request->getPost())) {
+            $messages = $form->getMessages();
+
+            foreach ($messages as $message) {
+                $this->getFlash()->message('error', $message);
+            }
+
+            return $this->response->redirect(array('for' => 'noticia.cadastrar'));
+        }
+
+        $date_created = date('Y-m-d H:i:s');
+
+        $noticia = new Noticia(array(
+            'titulo' => $this->request->get('titulo'),
+            'texto' => $this->request->get('texto'),
+            'data_cadastro' => $date_created,
+            'data_ultima_atualizacao' => $date_created,
+        ));
+
+        if (!$noticia->save()) {
+            $this->getFlash()->message('error', 'Houve um erro ao salvar a notícia. Por favor, tente novamente.');
+            return $this->response->redirect(array('for' => 'noticia.cadastrar'));
+        }
+
+        $this->getFlash()->message('success', 'Notícia cadastrada com sucesso');
+
         return $this->response->redirect(array('for' => 'noticia.lista'));
     }
 
-     public function excluirAction($id)
-     {
+    public function excluirAction($id) {
         return $this->response->redirect(array('for' => 'noticia.lista'));
-     }
+    }
+
+    /**
+     * 
+     * @return FlashSession
+     */
+    protected function getFlash() {
+        return $this->flash = $this->flash ?: new FlashSession();
+    }
 
 }
